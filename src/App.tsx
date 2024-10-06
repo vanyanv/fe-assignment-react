a4faed9d2e1837e9471613691ed8e76e9ac0f7c0
Vardan <vardan.vanyan@gmail.com>
2024-10-06 11:31:49 -0700

    added two buttom from MUI, created custom Styles for buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import Chart from "./Chart";
 import Table from "./Table";
 import useData from "./useData";
 import "./App.css";
+import { Button } from "@mui/material";
+
+const buttonStyle = {
+  fontSize: "14px",
+  color: "black", // black text
+  border: "1px solid black", // border
+  borderRadius: "0px", // makes border square
+  "&:hover": {
+    backgroundColor: "#9CBFD2", // hover effect for active button
+  },
+};
 
 const App = () => {
   const { data, loading } = useData();
@@ -15,6 +26,15 @@ const App = () => {
         <p>
           End Date: <input value="2020-01-07" />
         </p>
+      </div>
+      <div className="buttons-container">
+        <Button sx={buttonStyle} variant="outlined" size="medium">
+          Downloads
+        </Button>
+
+        <Button sx={buttonStyle} variant="outlined" size="medium">
+          Revenue
+        </Button>
       </div>
       <Chart data={data} loading={loading} />
       <Table data={data} loading={loading} />
